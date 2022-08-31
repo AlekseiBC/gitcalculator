d661f97557169e49c7f17cb18acaf38156f8f2b2
Aleksei <s2001175@edu.bc.fi>
2022-08-31 11:33:34 +0300

    lisätty erotus ja valinta summan ja erotuksen välillä

--- calculator/calculator/Program.cs
+++ calculator/calculator/Program.cs
@@ -10,8 +10,19 @@ namespace calculator
             int y = Convert.ToInt32(Console.ReadLine());
 
             int sum = x + y;
+            int erotus = x - y;
 
-            Console.WriteLine(sum);
+            Console.WriteLine("summa vai erotus?");
+
+            string vastaus = Console.ReadLine();
+
+            if (vastaus == "summa")
+            {
+                Console.WriteLine(sum);
+            } else if (vastaus == "erotus")
+            {
+                Console.WriteLine(erotus);
+            }
         }
     }
 }
